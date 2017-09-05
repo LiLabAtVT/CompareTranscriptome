2e4754df337fe3c740701a56901ef96e6c6e468e
jylee43 <jylee43@vt.edu>
2017-09-04 21:06:15 -0400

    install igraph, OrhtoClust dependency
    install.packages("igraph")
    I tried Rscript, but it's not working because of CRAN mirror

--- scripts/Section2.3_install_r_packages.R
+++ scripts/Section2.3_install_r_packages.R
@@ -3,6 +3,7 @@
 source('https://bioconductor.org/biocLite.R')
 biocLite('DESeq2')
 biocLite('edgeR')
+install.packages("igraph")	
 setwd("./software")
 install.packages("OrthoClust_1.0.tar.gz", repos=NULL, type="source")
 
